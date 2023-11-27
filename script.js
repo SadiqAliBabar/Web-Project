8caa5ec156c9d89c7d6f4e40fea224b07328e8df
SadiqAliBabar <sadiqalibabar@gmail.com>
2023-11-27 07:14:14 -0800

    cursor updated

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', function(e){
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+    });
 const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
